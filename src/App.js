0809de93e6e2d6be667277d4f3746c80f59c23bb
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-09-16 17:24:00 -0400

    main page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>Web Development - CS 5160</h1>
-			<ul>
-				<li>
-					<a href='/kanbas/home.html'></a>
-				</li>
-				<li>
-					<a href='/labs/a1/index.html'></a>
-				</li>
-			</ul>
+			<div>
+				<table border='1px solid black'>
+					<thead>
+						<th>Labs</th>
+						<th>Assignments</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<a href='/labs/a1/index.html'>Lab 1</a>
+							</td>
+							<td>
+								<a href='/kanbas/home.html'>A1</a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 	)
 }
