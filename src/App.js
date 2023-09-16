8ed775924538c5009b6b32a2911be934a2909afb
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-09-16 17:14:11 -0400

    added links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import "./App.css"
 function App() {
 	return (
 		<div className='App'>
-			<h1>Assignment 1</h1>
+			<h1>Web Development - CS 5160</h1>
+			<ul>
+				<li>
+					<a href='/kanbas/home.html'></a>
+				</li>
+				<li>
+					<a href='/labs/a1/index.html'></a>
+				</li>
+			</ul>
 		</div>
 	)
 }
