f472fe78cb9297ae826f4abe8492ee6468fc6b75
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-09-29 15:04:32 -0400

    main sidebar

--- src/App.js
+++ src/App.js
@@ -7,32 +7,25 @@ function App() {
 		<div className='App'>
 			<h1>Web Development - CS 5160</h1>
 			<h3>NUID: 002765803</h3>
-			<div>
-				<table border='1px solid black'>
-					<thead>
-						<th>Labs</th>
-						<th>Assignments</th>
-					</thead>
-					<tbody>
-						<tr>
-							<td>
-								<a href='/labs/a1'>Lab 1</a>
-							</td>
-							<td>
-								<a href='/kanbas'>A1</a>
-							</td>
-						</tr>
-						<tr>
-							<td>
-								<a href='/labs/a2'>Lab 2</a>
-							</td>
-							<td>
-								<a href='/kanbas'>A2</a>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+			<table border='1px solid black'>
+				<thead>
+					<th>Labs</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<a href='/labs/a1'>Lab 1</a>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href='/labs/a2'>Lab 2</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<br></br>
+			<strong>Kanbas Application : </strong> <a href='/kanbas'> Kanbas </a>
 		</div>
 	)
 }
