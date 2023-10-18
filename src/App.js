4bcbef9d0ab58b1fc31da814cb6f088e5f35f585
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-10-17 23:42:21 -0400

    start a3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 /** @format */
 
-import "./App.css"
+import Labs from "./Labs"
+import HelloWorld from "./Labs/a3/HelloWorld"
+import Kanbas from "./Kanbas"
+import { HashRouter } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router"
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>Web Development - CS 5160</h1>
-			<h3>NUID: 002765803</h3>
-			<table border='1px solid black'>
-				<thead>
-					<th>Labs</th>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							<a href='/labs/a1'>Lab 1</a>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<a href='/labs/a2'>Lab 2</a>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<br></br>
-			<strong>Kanbas Application : </strong> <a href='/kanbas'> Kanbas </a>
-		</div>
+		<HashRouter>
+			<div>
+				<Routes>
+					<Route path='/' element={<Navigate to='/Labs' />} />
+					<Route path='/hello' element={<HelloWorld />} />
+					<Route path='/Labs/*' element={<Labs />} />
+					<Route path='/Kanbas/*' element={<Kanbas />} />
+				</Routes>
+			</div>
+		</HashRouter>
 	)
 }
-
 export default App
