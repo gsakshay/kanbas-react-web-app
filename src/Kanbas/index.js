b786f35fb3eb3014c4380f2c579ccbf12750b2cf
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-10-18 21:00:06 -0400

    3.1 Kanbas Navigation

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,16 +1,12 @@
 /** @format */
 
+import KanbasNavigation from "./KanbasNavigation"
+
 function Kanbas() {
 	return (
 		<div className='d-flex'>
-			<div>
-				<h1>Kanbas Navigation</h1>
-			</div>
-			<div>
-				<h1>Account</h1>
-				<h1>Dashboard</h1>
-				<h1>Courses</h1>
-			</div>
+			<KanbasNavigation />
+			<div>Assignment</div>
 		</div>
 	)
 }
