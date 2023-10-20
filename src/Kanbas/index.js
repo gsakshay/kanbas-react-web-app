a7bcaf641a6a54a54b90db2ca84025e571715a38
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-10-19 20:07:31 -0400

    3.4 Course Navigation

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -5,6 +5,7 @@ import KanbasNavigation from "./KanbasNavigation"
 import { Routes, Route, Navigate } from "react-router"
 
 import "./index.css"
+import Courses from "./Courses"
 
 function Kanbas() {
 	return (
@@ -17,7 +18,7 @@ function Kanbas() {
 					<Route path='/' element={<Navigate to='Dashboard' />} />
 					<Route path='Account' element={<h1>Account</h1>} />
 					<Route path='Dashboard' element={<Dashboard />} />
-					<Route path='Courses/*' element={<h1>Courses</h1>} />
+					<Route path='Courses/:courseId*' element={<Courses />} />
 				</Routes>
 			</div>
 		</div>
