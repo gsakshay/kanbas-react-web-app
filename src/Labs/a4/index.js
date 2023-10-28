b883ac297f3c96dd600aa22a15b7df380b229453
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2023-10-27 21:30:43 -0400

    completed lab

--- src/Labs/a4/index.js
+++ src/Labs/a4/index.js
@@ -6,6 +6,14 @@ import ClickEvent from "./ClickEvent"
 import PassingDataOnEvent from "./PassingDataOnEvent"
 import PassingFunctions from "./PassingFunctions"
 import EventObject from "./EventObject"
+import Counter from "./Counter"
+import BooleanStateVariables from "./BooleanStateVariables"
+import StringStateVariables from "./StringStateVariables"
+import DateStateVariable from "./DateStateVariables"
+import ObjectStateVariable from "./ObjectStateVariable"
+import ArrayStateVariable from "./ArrayStateVariable"
+import ParentStateComponent from "./ParentStateComponent"
+import ReduxExamples from "./ReduxExamples"
 
 function Assignment4() {
 	function sayHello() {
@@ -15,11 +23,23 @@ function Assignment4() {
 		<div>
 			<h1>Assignment 4</h1>
 
+			<ReduxExamples />
+
+			<br />
+			<br />
+
 			<Add a={1} b={2} />
 			<ClickEvent />
 			<PassingDataOnEvent />
 			<PassingFunctions theFunction={sayHello} />
 			<EventObject />
+			<Counter />
+			<BooleanStateVariables />
+			<StringStateVariables />
+			<DateStateVariable />
+			<ObjectStateVariable />
+			<ArrayStateVariable />
+			<ParentStateComponent />
 		</div>
 	)
 }
